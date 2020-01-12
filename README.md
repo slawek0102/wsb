9a4a62197b25b363fad096b4cc6da61cabc144e5
Slawek0102 <grzebyk.slawomir@gmail.com>
2020-01-12 20:18:44 +0100

    add cypress

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 How to configure Storybook with React-Typescript
  -  https://dev.to/thechori/setting-up-storybook-with-typescript-in-create-react-app-26a0
  
+ Cypress with react-Typrescript
+ https://www.cypress.io/blog/2019/05/13/code-create-react-app-v3-and-its-cypress-tests-using-typescript/
  
---
+++ cypress/integration/spec.ts
@@ -0,0 +1,12 @@
+context('Example Cypress TodoMVC test', () => {
+  beforeEach(() => {
+    cy.visit('http://todomvc.com/examples/vue/')
+  })
+
+  it('adds 2 todos', function () {
+    cy.get('.new-todo')
+      .type('learn testing{enter}')
+      .type('be cool{enter}')
+    cy.get('.todo-list li').should('have.length', 2)
+  })
+})
